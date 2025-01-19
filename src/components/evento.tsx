e0f0dc689533b674e13f88091e554ba51fa034f7
Herbert <herberticleber@gmail.com>
2025-01-19 15:05:52 -0300

    botao maps

--- src/components/evento.tsx
+++ src/components/evento.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
+const mapUrl = {
+    src: 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d691.398462020139!2d-47.99893922098081!3d-15.999263941952776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTXCsDU5JzU3LjciUyA0N8KwNTknNTUuMSJX!5e1!3m2!1spt-BR!2sbr!4v1737258420134!5m2!1spt-BR!2sbr',
+    href: 'https://www.google.com/maps?q=-15.999283106991403,-47.998734435698346'
+}
+
 export default function Evento() {
     return (
         <div className="flex flex-col items-center justify-start min-h-screen px-4">
@@ -19,8 +24,24 @@ export default function Evento() {
                 priority
                 className="w-full h-full mb-10"
             />
-            <div className="border-verde border-solid border-4 rounded-xl overflow-hidden shadow-xl">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d691.398462020139!2d-47.99893922098081!3d-15.999263941952776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTXCsDU5JzU3LjciUyA0N8KwNTknNTUuMSJX!5e1!3m2!1spt-BR!2sbr!4v1737258420134!5m2!1spt-BR!2sbr" loading="lazy" className="h-[500px] w-full md:h-[450px] md:w-[600px]"></iframe>
+            <div className="w-full max-w-4xl mx-auto mb-12">
+                <div className="border-4 border-verde rounded-xl overflow-hidden shadow-xl">
+                    <iframe
+                        src={mapUrl.src}
+                        loading="lazy"
+                        className="h-[500px] w-full"
+                    ></iframe>
+                </div>
+                <div className="text-center mt-6">
+                    <a
+                        href={mapUrl.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block px-6 py-3 bg-verde text-white text-lg font-semibold rounded-lg shadow-md hover:bg-verde-escuro transition duration-300"
+                    >
+                        Ver no Google Maps
+                    </a>
+                </div>
             </div>
         </div>
     );
